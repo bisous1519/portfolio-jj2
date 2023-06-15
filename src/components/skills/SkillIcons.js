8fe196d3147ca0c495cbbd0a8107ff105faee2e0
bisous1519 <anipap2@naver.com>
2023-06-15 13:14:52 +0900

    Design: skill icon

--- src/components/skills/SkillIcons.js
+++ src/components/skills/SkillIcons.js
@@ -6,18 +6,44 @@ const SkillIconsBox = styled.ul`
   align-items: center;
   gap: 10px;
   & > li {
+    position: relative;
     width: 30px;
     height: 30px;
     border-radius: 10px;
-    overflow: hidden;
-    display: flex;
-    justify-content: center;
-    align-items: center;
     box-shadow: ${({ theme }) => theme.shadowColor};
-    & > img {
+    /* cursor: pointer; */
+    & > div {
       width: 100%;
       height: 100%;
-      object-fit: cover;
+      border-radius: 10px;
+      overflow: hidden;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      & > img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    }
+    & > span {
+      position: absolute;
+      left: 50%;
+      bottom: -24px;
+      transform: translateX(-50%);
+      font-size: ${({ theme }) => theme.fontSize.small};
+      background-color: ${({ theme }) => theme.textColor.gray};
+      color: ${({ theme }) => theme.textColor.reverse};
+      padding: 3px 5px 4px 5px;
+      border-radius: 5px;
+      opacity: 0;
+      transition: opacity 0.2s;
+      white-space: nowrap;
+    }
+    &:hover {
+      & > span {
+        opacity: 1;
+      }
     }
   }
 `;
@@ -28,7 +54,10 @@ export default function SkillIcons({ datas }) {
       {datas.length > 0 &&
         datas.map((data, idx) => (
           <li key={`iconitem-${idx}`}>
-            <img src={data.src} alt={data.alt} />
+            <div>
+              <img src={data.src} alt={data.alt} />
+            </div>
+            <span>{data.des}</span>
           </li>
         ))}
     </SkillIconsBox>
